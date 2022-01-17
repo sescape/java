ca051378df6e17779eac1ca0fda5919024d7b47a
sescape <sadafhsyed@gmail.com>
2022-01-17 17:39:24 -0500

    Update CmdLineArgs.java

--- coding_bootcamp/CmdLineArgs.java
+++ coding_bootcamp/CmdLineArgs.java
@@ -2,12 +2,12 @@ public class CmdLineArgs {
 	public static void main(String[] args) {
 		// verify if the length of argos is greater than 0
 		if (args.length <= 0) {
-		System.out.println("No arguments provided");
+			System.out.println("No arguments provided");
 		}
 
 		// iterate over and print the argos
 		for (String s : args) {
-		System.out.println(s);
+			System.out.println(s);
 		}
 	}
 }
